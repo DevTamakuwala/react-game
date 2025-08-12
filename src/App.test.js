089347a168e1e5239c3b9b346df95584a2c7b0d7
DevTamakuwala <dev.official2424@gmail.com>
2025-08-12 16:29:43 +0530

    Deployment configuration

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import Game from './Game';
+import App from './App';
 
 test('renders learn react link', () => {
-  render(<Game />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App />); // Render the App component
+  const nextPlayerText = screen.getByText(/Next player: Player 1/i); // Check for the initial game status text
+  expect(nextPlayerText).toBeInTheDocument(); // Assert that the text is present in the document
 });
